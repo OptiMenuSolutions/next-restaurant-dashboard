9a92a88f3f1cc9629019fc47e1e6b8c53029cd7f
OptiMenuSolutions <optimenu.solutions@gmail.com>
2025-08-29 17:12:11 +0000

    Add vercel analytics

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,16 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/client/dashboard": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/client/dashboard.js"
-    ],
-    "/client/login": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/client/login.js"
     ]
   },
   "ampFirstPages": []
