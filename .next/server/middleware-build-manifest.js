eb9a25c2bab0571ba20894b007bbf904d2980fd8
OptiMenuSolutions <optimenu.solutions@gmail.com>
2025-09-02 16:59:06 +0000

    Edit admin menu items page

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -20,16 +20,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/admin": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/admin.js"
-    ],
-    "/admin/clients": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/admin/clients.js"
-    ],
     "/admin/menu-items": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
