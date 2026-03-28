850f05836c8764ad1bf0bebd1254533ade96408d
OptiMenuSolutions <optimenu.solutions@gmail.com>
2026-03-28 22:33:21 +0000

    update _app

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,9 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+import { registerServiceWorker } from '../lib/registerSW';
+import { useEffect } from 'react';
+
+// Inside your App component:
+useEffect(() => { registerServiceWorker(); }, []);
