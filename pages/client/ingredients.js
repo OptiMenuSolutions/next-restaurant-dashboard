bdf6e4b162406b3cd366e3c2f3fa444e41635534
OptiMenuSolutions <optimenu.solutions@gmail.com>
2026-03-29 23:39:03 +0000

    add profile dropdown on ingredients

--- pages/client/ingredients.js
+++ pages/client/ingredients.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import supabase from '../../lib/supabaseClient';
 import { useWindowSize } from '../../lib/useWindowSize';
+import ProfileDropdown from '../../components/ProfileDropdown';
 
 // ─── Helpers ─────────────────────────────────────────────────────────────────
 
